ff525c5ca45dd5120a332470bf8bdd8863a9fa44
Snehal Sakre <sne.ringane@gmail.com>
2012-06-20 11:17:10 +0530

    second first commit to develope2

--- trywebscrap.rb
+++ trywebscrap.rb
@@ -18,58 +18,49 @@ class Recipedata < ActiveRecord::Base
 end 
 
 class Scrap
-page = Nokogiri::HTML(open("http://simplyrecipes.com/"))   
+def iterarelinks
+	page = Nokogiri::HTML(open("http://simplyrecipes.com/"))   
+
+	links = page.css('a')
+	hrefs = links.map{ |link|   link['href'] }
+	doc_hrefs = hrefs.select{ |href|  href.match("http://www.simplyrecipes.com/subject-index.php") != nil}
+
+	arr=[]
+	page1 = Nokogiri::HTML(open(doc_hrefs.to_s))
+	page1.search("//*[@id='content-inner']/div[1]/div/div/p[1]/a").each{|x| arr.push(x.attribute("href"))}
 
 
- links = page.css('a')
- puts links.length 
 
-hrefs = links.map{ |link|   link['href'] }
+	len = arr.length
+	arrALink=[]
+	for i in 0..len-1
+	page2 = Nokogiri::HTML(open(arr[i]))
+	page2.search("//*[@id='content-inner']/div[1]/div/div/div/div/div/div[2]/a").each{|z| arrALink.push(z.attribute("href"))}
+	end
 
+	arrALinkLen = arrALink.length
 
-doc_hrefs = hrefs.select{ |href|  href.match("http://www.simplyrecipes.com/subject-index.php") != nil}
-puts "new page is :#{doc_hrefs}"
-
-
-arr=[]
-page1 = Nokogiri::HTML(open(doc_hrefs.to_s))
-page1.search("//*[@id='content-inner']/div[1]/div/div/p[1]/a").each{|x| arr.push(x.attribute("href"))}
-
-
-puts len = arr.length
-
-arrALink=[]
-for i in 0..len-1
-#puts "link number : #{i}"
-page2 = Nokogiri::HTML(open(arr[i]))
-page2.search("//*[@id='content-inner']/div[1]/div/div/div/div/div/div[2]/a").each{|z| arrALink.push(z.attribute("href"))}
-end
-puts "content of arrALink are:"
-puts arrALink
-
-#arrRecipeLink=[]
-arrALinkLen = arrALink.length
-for j in 0..5
-#puts "link number : #{j}"
-page3 = Nokogiri::HTML(open(arrALink[j]))
-recipetitle = page3.search("//*[@id='entry-individual']/div[2]/span/h1").text
-puts "Title : "+recipetitle
-puts
-description = page3.search("//*[@id='recipe-intro']/p").text
-puts "Description :"+description
-puts
-ingredians = page3.search("//*[@id='recipe-ingredients']/ul").text
-puts "Ingredians : "+ingredians
-puts
-method = page3.search("//*[@id='recipe-method']").text
-puts "Method : "+method
-puts
-putdata = Recipedata.new(:Title=>recipetitle,:Description=>description,:Ingredians=>ingredians,:Method=>method)
-putdata.save
+	for j in 0..10
+	page3 = Nokogiri::HTML(open(arrALink[j]))
+	recipetitle = page3.search("//*[@id='entry-individual']/div[2]/span/h1").text
+	puts "Title : "+recipetitle
+	puts
+	description = page3.search("//*[@id='recipe-intro']/p").text
+	puts "Description :"+description
+	puts
+	ingredians = page3.search("//*[@id='recipe-ingredients']/ul").text
+	puts "Ingredians : "+ingredians
+	puts
+	method = page3.search("//*[@id='recipe-method']").text
+	puts "Method : "+method
+	puts
+	putdata = Recipedata.new(:Title=>recipetitle,:Description=>description,:Ingredians=>ingredians,:Method=>method)
+	putdata.save
+	end
 end
 end
 
-
+Scrap.new.iterarelinks
 
 
 
